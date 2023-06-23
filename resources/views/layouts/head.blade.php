31ae5fdc5a240baae4dbc6cf5e43c0a4ce368af2
Islam Aboukila <islam@Islams-MacBook-Pro.local>
2023-06-23 22:24:02 +0300

    adjusting rtl if the site is in arabic

--- resources/views/layouts/head.blade.php
+++ resources/views/layouts/head.blade.php
@@ -6,14 +6,23 @@
 <link href="{{URL::asset('assets/css/icons.css')}}" rel="stylesheet">
 <!--  Custom Scroll bar-->
 <link href="{{URL::asset('assets/plugins/mscrollbar/jquery.mCustomScrollbar.css')}}" rel="stylesheet"/>
+
+
+@if(Lang::locale() == 'ar')
+<link rel="stylesheet" href="{{URL::asset('assets/css-rtl/sidemenu.css')}}">
+<link href="{{URL::asset('assets/css-rtl/style.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+@else
 <!--  Sidebar css -->
 <link href="{{URL::asset('assets/plugins/sidebar/sidebar.css')}}" rel="stylesheet">
 <!-- Sidemenu css -->
-<link rel="stylesheet" href="{{URL::asset('assets/css-rtl/sidemenu.css')}}">
+<link rel="stylesheet" href="{{URL::asset('assets/css/sidemenu.css')}}">
 @yield('css')
 <!--- Style css -->
-<link href="{{URL::asset('assets/css-rtl/style.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/css/style.css')}}" rel="stylesheet">
 <!--- Dark-mode css -->
-<link href="{{URL::asset('assets/css-rtl/style-dark.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/css/style-dark.css')}}" rel="stylesheet">
 <!---Skinmodes css-->
-<link href="{{URL::asset('assets/css-rtl/skin-modes.css')}}" rel="stylesheet">
+<link href="{{URL::asset('assets/css/skin-modes.css')}}" rel="stylesheet">
+@endif
