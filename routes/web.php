01b5f8d3198687480525e9e296c1d503c9946444
Islam Aboukila <islam@Islams-MacBook-Pro.local>
2023-06-26 22:47:49 +0300

    v4 loggin and registration: installing npm and node

--- routes/web.php
+++ routes/web.php
@@ -17,4 +17,8 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
+
 Route::get('/{page}', [AdminController::class, 'index']);
