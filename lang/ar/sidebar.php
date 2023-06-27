45136c5fc442ca33cc4c8e30c30bae3c1d09d98a
Islam Aboukila <islam@Islams-MacBook-Pro.local>
2023-06-27 09:03:37 +0300

    v5 login and renaming sidebar

---
+++ lang/ar/sidebar.php
@@ -0,0 +1,20 @@
+<?php
+
+    return [
+        'app_name' => 'برنامج الفواتير',
+        'index' => 'الرئيسية',
+        'invoices' => 'الفواتير',
+        'invoices_list' => 'قائمة الفواتير',
+        'paid_invoices' => 'الفواتير المدفوعة',
+        'unpaid_invoices' => 'الفواتير الغير مدفوعة',
+        'paritaily_paid_invoices' => 'الفواتير المدفوعة جزئياً',
+        'reports' => 'التقارير',
+        'invoices_reports' => 'تقارير الفواتير',
+        'customers_reports' => 'تقارير العملاء',
+        'users' => 'المستخدمين',
+        'users_list' => 'قائمة المستخدمين',
+        'users_autorties' => 'صلاحيات المستخدمين',
+        'settings' => 'الاعدادات',
+        'add_dept' => 'اضافة قسم',
+        'add_product' => 'اضافة منتج',
+    ];
---
+++ lang/en/sidebar.php
@@ -0,0 +1,20 @@
+<?php
+
+    return [
+        'app_name' => 'Invoices App',
+        'index' => 'Main',
+        'invoices' => 'Invoices',
+        'invoices_list' => 'Invoices List',
+        'paid_invoices' => 'Paid Invoices',
+        'unpaid_invoices' => 'Unpaid Invoices',
+        'paritaily_paid_invoices' => 'Partially paid invoices',
+        'reports' => 'Reports',
+        'invoices_reports' => 'Invoices Reports',
+        'customers_reports' => 'Customer Reports',
+        'users' => 'Users',
+        'users_list' => 'Users List',
+        'users_autorties' => 'Users Eligibilities',
+        'settings' => 'Settings',
+        'add_dept' => 'Add Department',
+        'add_product' => 'Add Product',
+    ];
